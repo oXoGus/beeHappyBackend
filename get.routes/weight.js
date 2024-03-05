4e12176aff667bec54b04c9cc7b18685011a10d1
oXoGus <mdintrat0205@gmail.com>
2024-03-05 16:28:43 +0100

    test

--- get.routes/weight.js
+++ get.routes/weight.js
@@ -8,9 +8,27 @@ const router = express.Router() // on cherche l'url sur laquelle on est pour tra
 dotenv.config()
 
 router.get("/", (req, res) => {
-    res.status(200).json({message: "weight"})// on revoie un json contenant les datas
-})
+    // faire une route get pour l'API qui renvoie le poid de la ruche
+    const connection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: 'rucheDB',
+    }); // connection a la db
 
+    connection.connect((err) => {
+        if(err){
+            console.log("Erreur de connexion : " + err.stack)
+            res.send("Erreur de connexion : " + err.stack);
+            return;
+        }
+        console.log("connection a la base de donnée réussi !")
+        connection.query("SELECT poids FROM ruche", (err, rows) => {
+            if(err) throw err;
+            res.json({weight: rows[rows.length-1].poids}); // on envoie la derniere valeur de poids de la ruche
+        })
+    });
+})
 
 
 module.exports = router; // on renvoie l'url
